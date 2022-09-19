71592d900c1d4d2511c444bdde4602583fde03b9
lucassilva1010 <lucasaugusto.araripina@gmail.com>
2022-09-18 21:05:19 -0300

    Ultima aula do curso da Alura, de introdução a C#

---
+++ Aula-ForEncadeado/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Aula_ForEncadeado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Encadeando FOR");
+            Console.WriteLine("Imrpimindo uma cadeia de Cartarteres em Linas e Colunas");
+
+            for (int contadorLinhas=0; contadorLinhas<=10;contadorLinhas++)
+            {
+                for (int contadorColunas=0; contadorColunas<=contadorLinhas; contadorColunas++)
+                {
+                    //qui também poderia ser imIF,
+                    //para que pudesse testar(contadorColunas<=contadorLinhas)
+                    //ainde ser umtilizado um Break para pausar o teste
+                    Console.Write("*");//Essa saida é reposavel por colocar o *
+
+                }
+                Console.WriteLine("");// Essa saida é resposavel por o pulo de uma Linha!
+            }
+
+        }
+    }
+}
---
+++ AulaDoWhile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AulaDoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Apresentando o DO WHILE");
+
+            int a = 1, b=30;
+            do
+            {
+                Console.WriteLine(a);
+
+                a+=1;
+            } while (a>b);
+        }
+    }
+}
